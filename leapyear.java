1ff8a6b0d327f9647dd143d46a403b2bdfb19b3b
anukarki01 <anukarki1012@gmail.com>
2025-01-10 20:11:04 +0545

    Update leapyear.java

--- leapyear.java
+++ leapyear.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
  public class leapyear {
     public static void leap(int x) {
-        if (x % 100 != 0 || x % 400 == 0 || x % 4 == 0) {
+        if (x % 100 == 0 || x % 400 == 0 || x % 4 == 0) {
             System.out.println(x + " is a leap year.");}
          else {
                 System.out.println(x + " isn't a leap year.");
